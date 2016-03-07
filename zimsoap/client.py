95657cc9a7bfa8274dca71c049683f6251b58141
Emeric Million <emillion@oasiswork.fr>
2016-03-07 17:34:06 +0100

    Always return list for attributes

--- zimsoap/client.py
+++ zimsoap/client.py
@@ -518,7 +518,13 @@ class ZimbraAdminClient(ZimbraAbstractClient):
 
         return resp
 
+    # Config
+
     def get_all_config(self):
+        """
+        :returns: dict of attributes. Each attributes' key is a list of
+        attribute's value(s)
+        """
         resp = self.request_list('GetAllConfig')
         config = {}
         for attr in resp:
@@ -529,17 +535,20 @@ class ZimbraAdminClient(ZimbraAbstractClient):
                 else:
                     config[attr['n']].append(attr['_content'])
             else:
-                config[attr['n']] = attr['_content']
+                config[attr['n']] = [attr['_content']]
         return config
 
     def get_config(self, attr):
+        """
+        :returns: list of attribute's value(s)
+        """
         resp = self.request_list('GetConfig', {'a': {'n': attr}})
         if len(resp) > 1:
-            config = {attr: []}
+            config = []
             for a in resp:
-                config[attr].append(a['_content'])
+                config.append(a['_content'])
         elif len(resp) == 1:
-            config = {attr: resp[0]['_content']}
+            config = [resp[0]['_content']]
         else:
             raise KeyError('{} not found'.format(attr))
         return config
